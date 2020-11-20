9d106b920dfeee2c558a0990df41ba957056bca6
Jon amaya <amaya.jon95@gmail.com>
2020-11-19 20:14:45 -0600

    login stage fixed

--- IntelliJ_CS3365/src/hospitalSystem/LoginController.java
+++ IntelliJ_CS3365/src/hospitalSystem/LoginController.java
@@ -1,14 +1,19 @@
 package hospitalSystem;
 
+import Staff.StaffController;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 public class LoginController implements Initializable {
 
     LoginModuleModel loginModel = new LoginModuleModel();
@@ -22,6 +27,10 @@ public class LoginController implements Initializable {
     private ComboBox<Option> combobox;
     @FXML
     private Button loginButton;
+    @FXML
+    private Label loginstatus;
+
+
     public void initialize(URL url , ResourceBundle rb){
         if(this.loginModel.isDatabaseConnected()){
             this.dbstatus.setText("Connected to Database");
@@ -32,25 +41,57 @@ public class LoginController implements Initializable {
         this.combobox.setItems(FXCollections.observableArrayList(Option.values()));
 
     }
-    @FXML
-    public void Login(ActionEvent event){
 
+    @FXML
+    public void Login (ActionEvent eventt){
+        try{
+            if(this.loginModel.isLogin(this.username.getText(),this.password.getText(),((Option)this.combobox.getValue()).toString())){
+                Stage stage =(Stage)this.loginButton.getScene().getWindow();
+                stage.close();
+                switch (((Option)this.combobox.getValue()).toString()){
+                    case "Staff":
+                        staffLogin();;
+                        break;
+                    case "Nurse":
+                        staffLogin();
+                        break;
+
+                }
+            }else {
+                this.loginstatus.setText("Wrong Credentials");
+            }
+
+        }
+        catch (Exception localException){
+            localException.printStackTrace();
+
+        }
 
     }
 
     public void staffLogin(){
+        try{
+            Stage userStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = (Pane)loader.load(getClass().getResource("/Staff/staffXML.fxml").openStream());
+            StaffController staffController = (StaffController)loader.getController();
+
+            Scene scene = new Scene(root);
+            userStage.setScene(scene);
+            userStage.setTitle("Staff Menu");
+            userStage.setResizable(false);
+            userStage.show();
+
+
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
 
     }
 
-    public  void NurseLogin(){
+    public void nurseLogin(){
+
 
     }
 
-    public void doctorLogin(){
-
-    }
-    
-    public void CEOLogin(){
-
-    }
 }
